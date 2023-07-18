47824780249568ecf8369e0e0a0569f808cb39b6
asthnesis <athnesis@gmail.com>
2023-07-18 22:44:50 +0000

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,23 @@
-/**
-*Main: Entry point
-*Return: return 0
-*/
 #include "main.h"
-#include "stdio.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
 int main(void)
+
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
-return (0);
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
 }
