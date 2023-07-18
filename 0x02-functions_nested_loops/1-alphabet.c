325fbac748c27f2c97c137ed08e2603ac61b1780
asthnesis <athnesis@gmail.com>
2023-07-18 23:18:04 +0000

    3. islower

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-*Prints the alphabet in lowercase
+*print_alphabet - Prints the alphabet in lowercase
 *Return: void
 */
 void print_alphabet(void)
 {
 char c = 'a';
-while(c <= 'z')
+while (c <= 'z')
 {
 _putchar(c);
 c++;
