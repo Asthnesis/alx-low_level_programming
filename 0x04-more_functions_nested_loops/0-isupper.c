bfb9fcad0184f9a402d4b5b40292e2f0c7088f01
asthnesis <athnesis@gmail.com>
2023-07-20 19:59:53 +0000

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+* main: checks if c is uppercase
+* @c: number checked
+* return: 1 if uppercase and 0 if else
+*/
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
